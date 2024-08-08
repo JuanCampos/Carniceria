9546a2749712716d0a3f2fbd082095257387ff88
JuanCampos <117550029+JuanCampos@users.noreply.github.com>
2024-08-07 22:04:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el:".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView:1
+        },
+        520: {
+            slidesPerView:2
+        },
+        920: {
+            slidesPerView:3
+        }
+    }
+})
